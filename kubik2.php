337e19493fd5a54268fdd5cc199bcb74251585e5
afistapratama <42665945+afistapratama@users.noreply.github.com>
2018-08-24 12:50:58 +0700

    Add files via upload

---
+++ kubik2.php
@@ -0,0 +1,68 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+require_once("sdata-modules.php");
+/**
+ * @Author: Eka Syahwan
+ * @Date:   2017-12-11 17:01:26
+ * @Last Modified by:   Eka Syahwan
+ * @Last Modified time: 2018-08-17 15:13:34
+*/
+
+
+##############################################################################################################
+$config['deviceCode'] 		= '861111033669949';
+$config['tk'] 				= 'ACEWbTR_rIxLg7zIn3FKrKEfSDCJCQ9PbeVxdHRodw';
+$config['token'] 			= '670e1nvX6WK5SsjMY2UMM_EDoH_R2r2GJGfieewdwNIlU1y4rF5t134wE1_KRWQcHuZTtE_0tnw3cMk';
+$config['uuid'] 			= '93412c211da3482898f2d383a071ec16';
+$config['sign'] 			= '538e04251ae39e5639eda50455303ac6';
+$config['android_id'] 		= 'b313e4c0df45eb45';
+##############################################################################################################
+
+
+for ($x=0; $x <1; $x++) { 
+	$url 	= array(); 
+	for ($cid=0; $cid <20; $cid++) { 
+		for ($page=0; $page <10; $page++) { 
+			$url[] = array(
+				'url' 	=> 'http://api.beritaqu.net/content/getList?cid='.$cid.'&page='.$page,
+				'note' 	=> 'optional', 
+			);
+		}
+		$ambilBerita = $sdata->sdata($url); unset($url);unset($header);
+		foreach ($ambilBerita as $key => $value) {
+			$jdata = json_decode($value[respons],true);
+			foreach ($jdata[data][data] as $key => $dataArtikel) {
+				$artikel[] = $dataArtikel[id];
+			}
+		}
+		$artikel = array_unique($artikel);
+		echo "[+] Mengambil data artikel (CID : ".$cid.") ==> ".count(array_unique($artikel))."\r\n";
+	}
+	while (TRUE) {
+		$timeIn30Minutes = time() + 30*60;
+		$rnd 	= array_rand($artikel); 
+		$id 	= $artikel[$rnd];
+		$url[] = array(
+			'url' 	=> 'http://api.beritaqu.net/timing/read',
+			'note' 	=> $rnd, 
+		);
+		$header[] = array(
+			'post' => 'OSVersion=8.0.0&android_channel=google&android_id='.$config['android_id'].'&content_id='.$id.'&content_type=1&deviceCode='.$config['deviceCode'].'&device_brand=samsung&device_ip=114.124.239.'.rand(0,255).'&device_version=SM-A730F&dtu=001&lat=&lon=&network=wifi&pack_channel=google&time='.$timeIn30Minutes.'&tk='.$config['tk'].'&token='.$config['token'].'&uuid='.$config['uuid'].'&version=10047&versionName=1.4.7&sign='.$config['sign'], 
+		);
+		$respons = $sdata->sdata($url , $header); 
+		unset($url);unset($header);
+		foreach ($respons as $key => $value) {
+			$rjson = json_decode($value[respons],true);
+			echo "[+][".$id." (Live : ".count($artikel).")] Message : ".$rjson['message']." | Poin : ".$rjson['data']['amount']." | Read Second : ".$rjson['data']['current_read_second']."\r\n";
+			if($rjson[code] == '-20003' || $rjson['data']['current_read_second'] == '330' || $rjson['data']['amount'] == 0){
+				unset($artikel[$value[data][note]]);
+			}
+		}
+		if(count($artikel) == 0){
+			sleep(30);
+			break;
+		}
+		sleep(5);
+	}
+	$x++;
+}
